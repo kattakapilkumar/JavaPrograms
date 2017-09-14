3053364ddb984ac0e6f790a81c00844a0bf96485
Kapil Kumar Katta <kapilkatta123@gmail.com>
2017-09-14 17:26:15 +0530

    ToBinary

--- src/ToBinary.java
+++ src/ToBinary.java
@@ -11,12 +11,19 @@ public class ToBinary {
 		
 		
 		int b=0;
-		while(n>=0)
+		while(n!=0)
 		{
+			System.out.println("the n value is  "+n);
+			System.out.println("***********");
 			b=b*10;
+			System.out.println("now b value is "+b);
+			System.out.println("***********");
 			b=n%2;
+			System.out.println("now b value is "+b);
 			n=n/2;
 			b = b+b;
+			System.out.println("**********");
+			System.out.println("the b value is   "+b);
 			
 		}
 		  
